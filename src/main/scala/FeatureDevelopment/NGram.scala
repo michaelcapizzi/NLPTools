9ab579ea9275a6aea3aa3e26a977e03d3744ebbf
mcapizzi <mcapizzi@email.arizona>
2015-06-19 07:25:52 -0700

    added NGram class - withPadding not working

--- src/main/scala/FeatureDevelopment/NGram.scala
+++ src/main/scala/FeatureDevelopment/NGram.scala
@@ -12,36 +12,79 @@ class NGram(
     processor.annotate(textDoc)
   }
 
-  def getNGrams(gram: String, n: Int, punctuation: Boolean): Vector[String] = {
-    if (gram == "word") {
-      if (punctuation == false) {
-        val words = this.textDoc.sentences.map(_.words)       //get all words
-          .flatten                                            //remove sentence barriers
-          .filter(word => word.matches("[A-Za-z0-9]+"))       //remove punctuation
-          .toVector
-        words.iterator.sliding(n).toVector                    //get window of n
-          .map(ngram => ngram.mkString(" "))                  //convert to string
+  //TODO - figure out why withPadding isn't working
+  def getNGrams(gram: String, n: Int, punctuation: Boolean, withPadding: Boolean): Vector[String] = {
+    if (withPadding) {
+      if (gram == "word") {
+        if (punctuation == false) {
+          val words = this.textDoc.sentences.map(_.words) //get all words
+            .flatten //remove sentence barriers
+            .filter(word => word.matches("[A-Za-z0-9]+")) //remove punctuation
+            .toVector
+          words.iterator.sliding(n) //get window of n
+            .withPadding("#") //pads with #
+            .toVector
+            .map(ngram => ngram.mkString(" ")) //convert to string
+        } else {
+          val words = this.textDoc.sentences.map(_.words) //get all words
+            .flatten //remove sentence barriers
+            .toVector
+          words.iterator.sliding(n) //get window of n
+            .withPadding("#") //pads with #
+            .toVector
+            .map(ngram => ngram.mkString(" ")) //convert to string
+        }
       } else {
-        val words = this.textDoc.sentences.map(_.words)       //get all words
-          .flatten                                            //remove sentence barriers
-          .toVector
-        words.iterator.sliding(n).toVector                    //get window of n
-          .map(ngram => ngram.mkString(" "))                  //convert to string
+        if (punctuation == false) {
+          val lemmas = this.textDoc.sentences.map(_.lemmas.get.toArray) //get all lemmas
+            .flatten //remove sentence barriers
+            .filter(lemma => lemma.matches("[A-Za-z0-9]+")) //remove punctuation
+            .toVector
+          lemmas.iterator.sliding(n) //get window of n
+            .withPadding("#") //pads with #
+            .toVector
+            .map(ngram => ngram.mkString(" ")) //convert to string
+        } else {
+          val lemmas = this.textDoc.sentences.map(_.lemmas.get.toArray) //get all lemmas
+            .flatten //remove sentence barriers
+            .toVector
+          lemmas.iterator.sliding(n) //get window of n
+            .withPadding("#") //pads with #
+            .toVector
+            .map(ngram => ngram.mkString(" ")) //convert to string        }
+        }
       }
     } else {
-      if (punctuation == false) {
-        val lemmas = this.textDoc.sentences.map(_.lemmas.get.toArray)   //get all lemmas
-          .flatten                                                      //remove sentence barriers
-          .filter(lemma => lemma.matches("[A-Za-z0-9]+"))               //remove punctuation
-          .toVector
-        lemmas.iterator.sliding(n).toVector                   //get window of n
-          .map(ngram => ngram.mkString(" "))                  //convert to string
+      if (gram == "word") {
+        if (punctuation == false) {
+          val words = this.textDoc.sentences.map(_.words)       //get all words
+            .flatten                                            //remove sentence barriers
+            .filter(word => word.matches("[A-Za-z0-9]+"))       //remove punctuation
+            .toVector
+          words.iterator.sliding(n).toVector                    //get window of n
+            .map(ngram => ngram.mkString(" "))                  //convert to string
+        } else {
+          val words = this.textDoc.sentences.map(_.words)       //get all words
+            .flatten                                            //remove sentence barriers
+            .toVector
+          words.iterator.sliding(n).toVector                    //get window of n
+            .map(ngram => ngram.mkString(" "))                  //convert to string
+        }
       } else {
-        val lemmas = this.textDoc.sentences.map(_.lemmas.get.toArray)   //get all lemmas
-          .flatten                                                      //remove sentence barriers
-          .toVector
-        lemmas.iterator.sliding(n).toVector                   //get window of n
-          .map(ngram => ngram.mkString(" "))                  //convert to string
+        if (punctuation == false) {
+          val lemmas = this.textDoc.sentences.map(_.lemmas.get.toArray)   //get all lemmas
+            .flatten                                                      //remove sentence barriers
+            .filter(lemma => lemma.matches("[A-Za-z0-9]+"))               //remove punctuation
+            .toVector
+          lemmas.iterator.sliding(n).toVector                   //get window of n
+            .map(ngram => ngram.mkString(" "))                  //convert to string
+        } else {
+          val lemmas = this.textDoc.sentences.map(_.lemmas.get.toArray)   //get all lemmas
+            .flatten                                                      //remove sentence barriers
+            .toVector
+          lemmas.iterator.sliding(n).toVector                   //get window of n
+            .map(ngram => ngram.mkString(" "))                  //convert to string
+        }
       }
     }
   }
